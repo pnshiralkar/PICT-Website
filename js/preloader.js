4526112ba2c6e6b5c9352acadf95b7f44f0d2c49
Prathamesh Shiralkar <pnshiralkar@gmail.com>
2019-05-14 18:12:50 +0530

    Added Preloader and Principal and Trustee msg in index.html

---
+++ js/preloader.js
@@ -0,0 +1,5 @@
+$(window).on("load", function () {
+    $("#preloader-left").attr("class", $("#preloader-left").attr("class") + " preloader-loaded");
+    $("#preloader-right").attr("class", $("#preloader-right").attr("class") + " preloader-loaded");
+    $("#preloader-cont").attr("class", $("#preloader-cont").attr("class") + " preloader-cont-loaded");
+});
